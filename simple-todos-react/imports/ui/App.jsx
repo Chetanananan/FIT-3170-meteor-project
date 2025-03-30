651d854f8702954cd2f2ff4cfe8fefc7c2386fbe
Chetananan <csom0007@student.monash.edu>
2025-03-31 08:59:04 +1100

    Task 1.5

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Task } from './Task.jsx';
+
+const tasks = [
+  {_id: 1, text: 'Finish assignment'},
+  {_id: 2, text: 'Study for exam'},
+  {_id: 3, text: 'attend meeting'},
+];
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+
+    <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+    </ul>
   </div>
 );
