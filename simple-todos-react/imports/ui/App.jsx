7bb1e10bf225837aff7203396364651104606704
Chetananan <csom0007@student.monash.edu>
2025-03-31 11:45:25 +1100

    task 6.5

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useTracker, useSubscribe } from "meteor/react-meteor-data";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import { Task } from "./Task";
@@ -6,12 +6,24 @@ import { TaskForm } from "./TaskForm";
 
 export const App = () => {
   
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+  const [hideCompleted, setHideCompleted] = useState(false);
   const handleToggleChecked = ({ _id, isChecked }) =>
     Meteor.callAsync("tasks.toggleChecked", { _id, isChecked });
   const handleDelete = ({ _id }) =>
     Meteor.callAsync("tasks.delete", { _id });
   const isLoading = useSubscribe("tasks");
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  const tasks = useTracker(() => TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+    sort: { createdAt: -1 },
+  }).fetch());
+
+  const pendingTasksCount = useTracker(() =>
+    TasksCollection.find(hideCompletedFilter).count()
+  );
+
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
 
   if (isLoading()) {
     return <div>Loading...</div>;
@@ -21,13 +33,20 @@ export const App = () => {
       <header>
         <div className="app-bar">
           <div className="app-header">
-            <h1>To-do List</h1>
+          <h1>
+            📝️ To Do List
+            {pendingTasksTitle}
+          </h1>
           </div>
         </div>
       </header>
       <div className="main">
         <TaskForm />
-
+        <div className="filter">
+         <button onClick={() => setHideCompleted(!hideCompleted)}>
+           {hideCompleted ? 'Show All' : 'Hide Completed'}
+         </button>
+        </div>
         <ul className="tasks">
           {tasks.map((task) => (
             <Task
